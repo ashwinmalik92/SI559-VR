5655125ffb0358c94b80638a4125b380a3973257
Ashwin Malik <ashwinmalik92@gmail.com>
2020-10-15 16:15:38 -0400

    Audio scripting

--- custom.js
+++ custom.js
@@ -14,3 +14,37 @@ AFRAME.registerComponent('audiobench', {
         });
     }
 })
+
+AFRAME.registerComponent('audiotable', {
+    init: function () {
+        let playing = false;
+        let audio = this.el.components.sound;
+        this.el.setAttribute("src", "audio/table.ogg");
+        this.el.addEventListener('click', () => {
+            if (!playing) {
+                audio.playSound();
+            } else {
+                audio.stopSound();
+
+            }
+            playing = !playing;
+        });
+    }
+})
+
+AFRAME.registerComponent('audiolamp', {
+    init: function () {
+        let playing = false;
+        let audio = this.el.components.sound;
+        this.el.setAttribute("src", "audio/lamp.ogg");
+        this.el.addEventListener('click', () => {
+            if (!playing) {
+                audio.playSound();
+            } else {
+                audio.stopSound();
+
+            }
+            playing = !playing;
+        });
+    }
+})
