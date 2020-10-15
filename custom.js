5982ed3513348873d16e3317c78f95c73b7e5f3e
Ashwin Malik <ashwinmalik92@gmail.com>
2020-10-15 18:36:21 -0400

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,15 +1,9 @@
 AFRAME.registerComponent('audiobench', {
     init: function () {
-        let playing = false;
         let audio = this.el.components.sound;
         this.el.setAttribute("src", "audio/bench.ogg");
         this.el.addEventListener('click', () => {
-            if (!playing) {
-                audio.playSound();
-            } else {
-                audio.stopSound();
-            }
-            playing = !playing;
+            audio.playSound();
         });
     }
 })
