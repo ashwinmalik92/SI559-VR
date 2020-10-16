2024071cb7d11ab8a02b80526778314f04196790
Ashwin Malik <ashwinmalik92@gmail.com>
2020-10-15 22:27:01 -0400

    Update main.js

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ AFRAME.registerComponent('spheretracker', {
         let solved = false;
         this.el.addEventListener('click', () => {
             if (solved === false) {
-                var textString = document.getElementById('learn-tracker').getAttribute("text")['value'];
+                var textString = document.getElementById('learn-tracker').getAttribute("text")['value'].toString();
                 console.log(textString);
                 var newNum = textString.substring(-3, 1).parseInt() + 1;
-                var newString = newNum.parseInt();
+                var newString = newNum.toString();
                 console.log(newString);
                 newString = "font: https://cdn.aframe.io/fonts/Aileron-Semibold.fnt; width: 18; anchor: center; align: center; value: Learned " + newString + "/3";
                 document.getElementById('learn-tracker').setAttribute("text", newString);
