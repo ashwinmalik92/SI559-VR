6917462eaf4d920a6813a0d342fef0cd4c7c17e8
Ashwin Malik <ashwinmalik92@gmail.com>
2020-10-15 22:41:24 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,36 @@
+AFRAME.registerComponent('cubetracker', {
+    init: function () {
+        let solved = false;
+        this.el.addEventListener('click', () => {
+            if (solved === false) {
+                var textString = document.getElementById('learn-tracker').getAttribute("text")['value'].toString();
+                var newNum = Number(textString.substring(8, 9)) + 1;
+                var newString = newNum.toString();
+                newString = "font: https://cdn.aframe.io/fonts/Aileron-Semibold.fnt; width: 18; anchor: center; align: center; value: Learned " + newString + "/3";
+                document.getElementById('learn-tracker').setAttribute("text", newString);
+                solved = true;
+            }
+        });
+    }
+})
+
 AFRAME.registerComponent('spheretracker', {
+    init: function () {
+        let solved = false;
+        this.el.addEventListener('click', () => {
+            if (solved === false) {
+                var textString = document.getElementById('learn-tracker').getAttribute("text")['value'].toString();
+                var newNum = Number(textString.substring(8, 9)) + 1;
+                var newString = newNum.toString();
+                newString = "font: https://cdn.aframe.io/fonts/Aileron-Semibold.fnt; width: 18; anchor: center; align: center; value: Learned " + newString + "/3";
+                document.getElementById('learn-tracker').setAttribute("text", newString);
+                solved = true;
+            }
+        });
+    }
+})
+
+AFRAME.registerComponent('cylindertracker', {
     init: function () {
         let solved = false;
         this.el.addEventListener('click', () => {
